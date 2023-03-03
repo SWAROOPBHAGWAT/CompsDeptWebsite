ddc56131c5d77e35131934b5449e29afd1659b4a
Prakhar Sharma <prakhar29sharma@gmail.com>
2023-03-03 22:05:04 +0530

    Added Others page to navbar dropdown

--- temp.js
+++ temp.js
@@ -153,6 +153,7 @@ nav.innerHTML = `<div class="container-fluid">
                 <li><a class="dropdown-item" href="/cocurricular/industrial_visit.html">Industrial Visits</a></li>
                 <li><a class="dropdown-item" href="/cocurricular/events.html">Certification & Workshop programs</a></li>
                 <li><a class="dropdown-item" href="/cocurricular/programming_contest.html">Programming Contests</a></li>
+                <li><a class="dropdown-item" href="/cocurricular/others.html">Others</a></li>
                 </ul>
               </li>
               <li class="nav-item dropdown text-center">
